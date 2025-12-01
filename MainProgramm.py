ca59e9b3256e298496f2f1698104e0b7352920ee
YeneralC <sacha.lecamus18@gmail.com>
2025-12-01 11:49:33 +0100

    Quinze v3

--- MainProgramm.py
+++ MainProgramm.py
@@ -1,54 +1,4 @@
-
-
-def main():
-    print("Bien le bonsoir cher caramade!")
-    print("On va s'entraîner à faire la guerre froide")
-    J1 = input("Quel est le nom du premier joueur jouant les USA? ")
-    J2 = input("Quel est le nom du second joueur jouant l'URSS? ")
-    print(f"Bienvenue {J1} et {J2}! On se positionne à Cuba en 1962.")
-    print("Les règles sont simples: taper les communistes avant qu'ils ne vous tapent!")
-    print('Vous disposez d\'un sous marin de 3 cases de long, 1 de 2 cases de long et 1 de 1 case de long.')
-
-    # Demander les positions des sous-marins
-    positions = {}
-    print("================================")
-    print("\n\nCommençons par positionner vos sous-marins.")
-    print("Ne regardez pas l'écran pendant que l'autre joueur entre ses positions.")
-    for joueur in [J1, J2]:
-        positions[joueur] = {}
-        print(f'Joueur {joueur}, c\'est à votre tour de positionner vos sous-marins.')
-        print("Veuillez faire en sort de ne pas dépasser la grille de 10x5 cases et de ne pas superposer les sous-marins.")
-        print('Veuillez entrer les positions de vos sous marins au format "Profondeur A1 A2 A3" pour le sous marin de 3 cases, etc.')
-        print('Exemple: "100 A1 A2 A3" pour un sous marin de 3 cases en profondeur 100m')
-        print('Vous pouvez choisir les profondeurs suivantes: 100m, 200m, 300m')
-        while True:
-            sub3 = input(f"{joueur}, entrez les positions de votre sous marin de 3 cases: ")
-            sub3 = sub3.split(" ")
-            if position_correcte(sub3, 3, positions[joueur]):
-                positions[joueur]['3cases'] = sub3
-                break
-            else:
-                print("Positions incorrectes, veuillez réessayer.")
-        while True:
-            sub2 = input(f"{joueur}, entrez les positions de votre sous marin de 2 cases: ")
-            sub2 = sub2.split(" ")
-            if position_correcte(sub2, 2, positions[joueur]):
-                positions[joueur]['2cases'] = sub2
-                break
-            else:
-                print("Positions incorrectes, veuillez réessayer.")
-        while True:
-            sub1 = input(f"{joueur}, entrez la position de votre sous marin de 1 case: ")
-            sub1 = sub1.split(" ")
-            if position_correcte(sub1, 1, positions[joueur]):
-                positions[joueur]['1cases'] = sub1
-                break
-            else:
-                print("Positions incorrectes, veuillez réessayer.")
-        print("Vos sous-marins ont été positionnés avec succès!")
-        print(50 * '\n')
-    print(positions)
-
+import grid
 
 def position_correcte(position, nbcases, dico_positions):
     # Vérifier que les positions sont dans la grille 10x5 et ne se chevauchent pas
@@ -104,6 +54,55 @@ def position_correcte(position, nbcases, dico_positions):
                 return False
     return True
 
+
+def main():
+    print("Bien le bonsoir cher camarade!")
+    print("On va s'entraîner à faire la guerre froide")
+    J1 = input("Quel est le nom du joueur jouant les USA? ")
+    J2 = input("Quel est le nom du joueur jouant l'URSS? ")
+    print(f"Bienvenue {J1} et {J2}! On se positionne à Cuba en 1962.")
+    print("Les règles sont simples: taper les communistes avant qu'ils ne vous tapent!")
+    print('Vous disposez d\'un sous marin de 3 cases de long, 1 de 2 cases de long et 1 de 1 case de long.')
+
+    # Demander les positions des sous-marins
+    positions = {}
+    print("================================")
+    print("\n\nCommençons par positionner vos sous-marins.")
+    print("Ne regardez pas l'écran pendant que l'autre joueur entre ses positions.")
+    for joueur in [J1, J2]:
+        positions[joueur] = {}
+        print(f'Joueur {joueur}, c\'est à votre tour de positionner vos sous-marins.')
+        print("Veuillez faire en sort de ne pas dépasser la grille de 10x5 cases et de ne pas superposer les sous-marins.")
+        print('Veuillez entrer les positions de vos sous marins au format "Profondeur A1 A2 A3" pour le sous marin de 3 cases, etc.')
+        print('Exemple: "100 A1 A2 A3" pour un sous marin de 3 cases en profondeur 100m')
+        print('Vous pouvez choisir les profondeurs suivantes: 100m, 200m, 300m')
+        while True:
+            sub3 = input(f"{joueur}, entrez les positions de votre sous marin de 3 cases: ")
+            sub3 = sub3.split(" ")
+            if position_correcte(sub3, 3, positions[joueur]):
+                positions[joueur]['3cases'] = sub3
+                break
+            else:
+                print("Veuillez réessayer.")
+        while True:
+            sub2 = input(f"{joueur}, entrez les positions de votre sous marin de 2 cases: ")
+            sub2 = sub2.split(" ")
+            if position_correcte(sub2, 2, positions[joueur]):
+                positions[joueur]['2cases'] = sub2
+                break
+            else:
+                print("Veuillez réessayer.")
+        while True:
+            sub1 = input(f"{joueur}, entrez la position de votre sous marin de 1 case: ")
+            sub1 = sub1.split(" ")
+            if position_correcte(sub1, 1, positions[joueur]):
+                positions[joueur]['1cases'] = sub1
+                break
+            else:
+                print("Veuillez réessayer.")
+        print("Vos sous-marins ont été positionnés avec succès!")
+        print(225 * '\n')
+    print(positions)
 
 if __name__ == "__main__":
     main()
